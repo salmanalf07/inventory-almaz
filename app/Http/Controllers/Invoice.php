541495c217d123c80a4662e82cc7d3c9c2adeeca
salmanalf07 <salmanalf07@gmail.com>
2023-07-07 10:33:02 +0700

    edit rekap invoice

--- app/Http/Controllers/Invoice.php
+++ app/Http/Controllers/Invoice.php
@@ -355,7 +355,7 @@ class Invoice extends Controller
                             $temp[$datan->DetailSJ['date_sj'] . "*" . $datan->DetailSJ['nosj'] . "*" . $datan->part_id . "*"]["price"] = 0;
                         }
                         $temp[$datan->DetailSJ['date_sj'] . "*" . $datan->DetailSJ['nosj'] . "*" . $datan->part_id . "*"]["qty"] += $datan->qty;
-                        $temp[$datan->DetailSJ['date_sj'] . "*" . $datan->DetailSJ['nosj'] . "*" . $datan->part_id . "*"]["price"] += $datan->total_price;
+                        $temp[$datan->DetailSJ['date_sj'] . "*" . $datan->DetailSJ['nosj'] . "*" . $datan->part_id . "*"]["price"] += floatval($datan->total_price);
                     }
                 }
 
